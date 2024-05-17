987625296af1f1c856a4058405ef8eee78ca07f8
khanh270504 <124353786+khanh270504@users.noreply.github.com>
2024-05-17 17:16:06 +0700

    update

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-	private static final String URL = "jdbc:sqlserver://89P13:1433;databaseName=PLAYER_POINT; encrypt = true;trustServerCertificate = true";
+	private static final String URL = "jdbc:sqlserver://DESKTOP-M57B9H4:1433;databaseName=QLPoints; encrypt = true;trustServerCertificate = true";
 	private static final String USERNAME = "sa";
-	private static final String PASSWORD = "123456";
+	private static final String PASSWORD = "anh270504";
 
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
